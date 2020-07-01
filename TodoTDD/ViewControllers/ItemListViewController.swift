aa6dd9ebf258a4341c565545e946243af38a4b23
이현호 <mizzking75@gmail.com>
2020-07-01 19:45:15 +0900

    Add ItemListViewController

---
+++ TodoTDD/ViewControllers/ItemListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListViewController.swift
+//  TodoTDD
+//
+//  Created by 이현호 on 2020/07/01.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ TodoTDDTests/ViewControllers/ItemListViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  ItemListViewControllerTests.swift
+//  TodoTDDTests
+//
+//  Created by 이현호 on 2020/07/01.
+//  Copyright © 2020 tempYsoup. All rights reserved.
+//
+
+import Foundation
