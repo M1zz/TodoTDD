c284e3dd5cb1d3542ecc4ea9f9a41141471f3e37
hyunho.lee <mizzking75@gmail.com>
2021-03-06 18:39:33 +0900

    Fix inputViewController, Add DetailViewController

--- TodoTDD/ViewControllers/InputViewController.swift
+++ TodoTDD/ViewControllers/InputViewController.swift
@@ -7,13 +7,72 @@
 //
 
 import UIKit
+import CoreLocation
 
-class InputViewController: UIViewController {
+class InputViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    lazy var geocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleTextField.delegate = self
+        addressTextField.delegate = self
+        descriptionTextField.delegate = self
     }
     
-    @IBAction func cancel() {
-      dismiss(animated: true, completion: nil)
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      view.endEditing(true)
+    }
+    
+    
+    @IBAction func TapCancelButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func tapOkButton(_ sender: Any) {
+        guard let titleString = titleTextField.text,
+          titleString.count > 0 else {
+            return
+        }
+        
+        var date: Date?
+        if datePicker != nil {
+            date = datePicker.date
+        }
+        
+        var descriptionString: String?
+        if descriptionTextField != nil {
+            descriptionString = descriptionTextField.text
+        }
+        
+        var placeMark: CLPlacemark?
+        var addressName: String?
+
+        if addressTextField != nil {
+            addressName = addressTextField.text
+            addressName = "경기도 용인시 수지구 도마치로89번길 16"
+            if let addressName = addressName, addressName.count > 0 {
+                
+                geocoder.geocodeAddressString(addressName) { [weak self] placeMarks, _ in
+                    placeMark = placeMarks?.first
+                    
+                    // TODO:- Add model
+                    
+                    
+                    DispatchQueue.main.async {
+                      //self?.itemManager?.add(item)
+                      self?.dismiss(animated: true)
+                    }
+                }
+            }
+        }
     }
 }
